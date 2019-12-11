217dcbfc792376020e6679fcf8239a5f80b558f0
WinkPascal <46743303+WinkPascal@users.noreply.github.com>
2019-12-11 12:41:52 +0100

    ...

---
+++ src/main/java/hu/pafr/richrail/CommandTest.java
@@ -0,0 +1,9 @@
+package hu.pafr.richrail;
+
+import hu.pafr.richrail.parser.RichRailCli;
+
+public class CommandTest {
+	public static void main(String[] args) throws CloneNotSupportedException {
+		RichRailCli.voerCommandUit("new train id");
+	}
+}
