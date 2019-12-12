f1656324672fabb29a73a5eb3c2d3ae443b94431
WinkPascal <46743303+WinkPascal@users.noreply.github.com>
2019-12-12 16:52:33 +0100

    .

--- src/main/java/hu/pafr/richrail/Gui/GUIcmd.java
+++ src/main/java/hu/pafr/richrail/Gui/GUIcmd.java
@@ -1,36 +1,47 @@
 package hu.pafr.richrail.Gui;
 
-
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.event.*;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class GUIcmd extends Application{
-	
-	private Scene scene;
-	private Group group;
-	private TextField spoor;
+public class GUIcmd extends Application {
+	// Adding a Label
+	Label label1 = new Label("Name:");
+	TextField textField = new TextField();
+	HBox hb = new HBox();hb.getChildren().addAll(label1,textField);hb.setSpacing(10);
 
-	
+	// Setting an action for the Submit button
+	submit.setOnAction(new EventHandler<ActionEvent>(){
+
+	public void handle(ActionEvent e) {
+	        if ((comment.getText() != null && !comment.getText().isEmpty())) {
+	            label.setText(name.getText() + " " + lastName.getText() + ", "
+	                + "thank you for your comment!");
+	        } else {
+	            label.setText("You have not left a comment.");
+	        }
+	     }
+	 });
+	 
+	//Setting an action for the Clear button
+	clear.setOnAction(new EventHandler<ActionEvent>() {
+
 	@Override
-	public void start(Stage window) throws Exception{
+	    public void handle(ActionEvent e) {
+	        name.clear();
+	        lastName.clear();
+	        comment.clear();
+	        label.setText(null);
+	    }
+	});
+	@Override
+	public void start(Stage arg0) throws Exception {
 		
-		spoor = new TextField();
-		spoor.setPrefColumnCount(50);
-		spoor.setPrefHeight(200);
-		group = new Group(spoor);
-	
-		scene = new Scene(group,  900, 900);
-		
-		window.setTitle("RichRail");
-		window.setScene(scene);
-		window.show();
-		
-	}
-
-	public static void main (String[] args) {
-		launch(args);
 	}
 }
+
+
