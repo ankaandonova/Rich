42164ceb834444245a24f44b1e4721a6b354889b
ankaandonova <anka.andonova@student.hu.nl>
2019-12-13 20:03:14 +0100

    cloneeeeeeee

--- src/main/java/hu/pafr/richrail/Gui/GUItest.java
+++ src/main/java/hu/pafr/richrail/Gui/GUItest.java
@@ -2,22 +2,16 @@ package hu.pafr.richrail.Gui;
 
 import java.io.FileNotFoundException;
 import java.lang.reflect.InvocationTargetException;
-
-import hu.pafr.richrail.database.LocomotiefDao;
-import hu.pafr.richrail.database.LocomotiefDaoImpl;
 import hu.pafr.richrail.locomotief.Builder;
 import hu.pafr.richrail.locomotief.Locomotief;
 import hu.pafr.richrail.locomotief.LocomotiefBuilder;
-import hu.pafr.richrail.spoor.Spoor;
 import hu.pafr.richrail.wagon.Wagon;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -25,24 +19,22 @@ public class GUItest extends Application {
 
 	private Scene scene;
 	private BorderPane schermBorder;
-	private static HBox scherm;
 	private static VBox scherm1;
 
 	@Override
 	public void start(Stage window) throws Exception, InvocationTargetException {
 
 		schermBorder = new BorderPane();
-
-		// draw
-		scherm = createHBox();
-
 		scherm1 = creatVBox();
-		schermBorder.setTop(scherm);
+		
+		//train
+		
+		
 		schermBorder.setTop(scherm1);
 		// spoor
 		schermBorder.setLeft(GUISpoor.createSpoorKeuzeMenu());
 
-		// trein
+		//locomotief
 		schermBorder.setCenter(GUIlocomotief.createLocomotiefKeuzeMenu());
 
 		// wagon
@@ -63,44 +55,32 @@ public class GUItest extends Application {
 	 * spoor.getLocomotiefenFromDatabase();
 	 * 
 	 * for (Locomotief locomotief : spoor.getLocomotiefen()) { Image
-	 * benzineLocomotiefImg = new Image("locomotief.jpg");
+	 * benzineLocomotiefImg = new Image("slaapwagon.jpg");
 	 * scherm1.getChildren().add(new ImageView(benzineLocomotiefImg));
-	 * //.createWagon(locomotief.getNaam()); } }
+	 * createWagon(locomotief.getNaam()); } }
 	 */
 	static void createWagon(String locomotiefNaam) throws FileNotFoundException {
-		System.out.println("==============================");
+		
 		Builder builder = new LocomotiefBuilder();
 		builder.setNaam(locomotiefNaam);
-		Locomotief locomotief= builder.build();
+		Locomotief locomotief = builder.build();
 		locomotief.getWagonnenFromDatabase();
-		
-		System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^" + locomotief.getWagons());
+
 		for (Wagon wagon : locomotief.getWagons()) {
 			if (wagon.getBedden() > 0) {
-			
+
 				Image slaapWagonImg = new Image("slaapwagon.jpg");
 				scherm1.getChildren().add(new ImageView(slaapWagonImg));
 			} else if (wagon.getStoelen() > 0) {
-		
+
 				Image personenWagonImg = new Image("personenwagon.jpg");
 				scherm1.getChildren().add(new ImageView(personenWagonImg));
 			} else {
-				
+
 				Image transportWagonfImg = new Image("transportwagon.jpg");
 				scherm1.getChildren().add(new ImageView(transportWagonfImg));
 			}
 		}
-	}
-
-	@SuppressWarnings("static-access")
-	private HBox createHBox() {
-		HBox hbox = new HBox();
-		schermBorder.setMargin(hbox, new Insets(5));
-		hbox.prefWidthProperty().bind(schermBorder.widthProperty());
-		hbox.prefHeightProperty().bind(schermBorder.heightProperty().subtract(500));
-		hbox.setStyle("-fx-border-style: dotted; -fx-border-width: 0 0 1 0 ; -fx-font-weight: bold;");
-		hbox.setAlignment(Pos.BASELINE_CENTER);
-		return hbox;
 	}
 
 	protected static VBox creatVBox() {
